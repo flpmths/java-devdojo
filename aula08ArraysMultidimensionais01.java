487c4b7a2e4779a752194245aa1a10633717385f
flpmths <fellipee_m@hotmail.com>
2026-04-08 10:02:53 -0300

    Estudos ArraysMultidimensionais

--- aula08ArraysMultidimensionais01.java
+++ aula08ArraysMultidimensionais01.java
@@ -2,7 +2,29 @@ package devDojo;
 
 public class aula08ArraysMultidimensionais01 {
     static void main(String[] args) {
-        //
-        
+        // 1,2,3,4,5 Meses
+        // 31,28,31,30 Dias
+
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) { //em dias estava com 0, é o mesmo que ter colocado 3 em dias[0].length
+                System.out.println(dias[i][j]);
+
+            }
+        }
+        System.out.println("-----------------------");
+        for (int[] arrBase: dias){
+            for(int num:arrBase){
+                System.out.println(num);
+            }
+        }
     }
 }
---
+++ aula08ArraysMultidimensionais02.java
@@ -0,0 +1,18 @@
+package devDojo;
+
+public class aula08ArraysMultidimensionais02 {
+    static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[3];
+        arrayInt[2] = new int[6];
+
+        for(int[] arrayBase: arrayInt){
+            System.out.println("\n-------");
+            for (int num: arrayBase){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
